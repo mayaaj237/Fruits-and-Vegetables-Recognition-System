331aee3ed4e7c74b429b68d5e45529f8ee8ab89e
mayaaj237 <91125063+mayaaj237@users.noreply.github.com>
2024-11-29 00:24:58 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='Fruits and Vegetables Recognition System',
+    version='0.0.1',
+    author='maayaj',
+    author_email='mjadhao4u@gmail.com',
+    packages=find_packages(),
+    install_requires=['pandas','numpy','seaborn']
+)
